ac1535bee25b840ca7e7872276891da455b04fc1
merpich <merpich.dev@gmail.com>
2022-07-28 19:33:09 +0300

    fixed mobile menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const body = document.body;
 const header = document.querySelector('.header');
 const headerInner = document.querySelector('.header__inner');
 const headerBurger = document.querySelector('.header__burger');
@@ -8,6 +9,7 @@ const headerNav = document.querySelector('.header__nav');
 headerBurger.addEventListener('click', () => {
 	headerBurger.classList.toggle('is-active');
 	headerNav.classList.toggle('is-open');
+	body.classList.toggle('is-lock');
 });
 
 window.addEventListener('scroll', () => {
